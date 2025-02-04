ae40bb168be33c95953691e57bdae39d88b3b097
mrphaox <oscar.neira@usantoto.edu.co>
2025-02-03 19:09:09 -0500

    s

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  experimental: {
-    appDir  : true,
-  },
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
